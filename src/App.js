06e50deae54bdc2108b2fcc77516bd2d76098b21
Konsor <Austin.Konsor@us.sogeti.com>
2020-01-29 14:22:56 -0500

    Connect with express api,react and bootsrap styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/getSogetiEmployees')
+    .then(employees => employees.json())
+    .then(_employees => {
+      console.log(_employees)
+      this.setState({
+        employees: _employees
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App container">
+      <div className="jumbotron">
+          <h1 className="display-4">GOAT Starting 5</h1>
+          <p className="lead">Top athletes IMO, including myself.</p>
+          <hr className="my-4"/>
+          <p>Name a better 5.</p>
+        </div>
+        <div className="dropdown mb-3">
+        <a className="btn btn-secondary dropdown-toggle" href="\#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
         </a>
-      </header>
-    </div>
-  );
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" href="/#">Action</a>
+          <a className="dropdown-item" href="/#">Another action</a>
+          <a className="dropdown-item" href="/#">Something else here</a>
+        </div>
+      </div>
+        <div className ="row">
+        { this.state.employees.map((employee, index) => {
+          return (
+            <div className="media col-12 mb-4 p-3 directory-entry" key = {index}>
+              <img src={employee.img} className="mr-3 directory-image" alt="..."/>
+              <button type="button" className="btn btn-outline-primary">View</button>
+                <div className="media-body">
+                  <h5 className="mt-0">{employee.name}</h5>
+                  {employee.location}
+              </div>
+          </div>
+          )
+        })
+      }
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
